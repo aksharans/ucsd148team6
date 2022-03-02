05e87391d9c935b7abc89af1814ab6638f46bee2
aksharans <aksharan@gmail.com>
2022-03-01 20:38:04 -0800

    testing actuator values

--- scripts/target_detection_node.py
+++ scripts/target_detection_node.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.Node import Node
 from std_msgs.msg import Float32
 from std_msgs.msg import Image
+from geometry_msgs.msg import Twist
+
 
 NODE_NAME = 'target_detection_node'
 
@@ -12,22 +14,50 @@ CAMERA_IMG_TOPIC_NAME = '/camera/color/image_raw'
 STEERING_TOPIC_NAME = '/steering'
 THROTTLE_TOPIC_NAME = '/throttle'
 SERVO_TOPIC_NAME = '/servo'
-
+ADAFRUIT_TOPIC_NAME = '/cmd_vel'
 
 class TargetDetection(Node):
 
     def __init__(self):
         super().__init__(NODE_NAME)
     
-        self.steering_publisher = self.create_publisher(Float32, STEERING_TOPIC_NAME, 10)
-        self.throttle_publisher = self.create_publisher(Float32, THROTTLE_TOPIC_NAME, 10)
         self.servo_publisher = self.create_publisher(Float32, SERVO_TOPIC_NAME, 10)
+
+        self.adafruit_publisher = self.create_publisher(Twist, ADAFRUIT_TOPIC_NAME, 10)
+        # steering: channel x, throttle: channel y
+        # self.steering_publisher = self.create_publisher(Float32, STEERING_TOPIC_NAME, 10)
+        # self.throttle_publisher = self.create_publisher(Float32, THROTTLE_TOPIC_NAME, 10)
 
         self.camera_subscriber = self.create_subscription(Image, CAMERA_IMG_TOPIC_NAME, self.controller, 10)
     
     def controller(self, data):
+
+
         # cv2 image processing
         # publish to steering & constant throttle
+
+    
+        # change steering to channel 4 and throttle to channel 7 in adafruit_twist.py
+
+        # throttle:
+        # linear.x: 0.1 (netural), 0.2 (medium forward)
+
+        # steering:
+        # angular.z: max-left (not actual max, but to be safe) -0.3
+        # angular.z: max-right (not actual max, but to be safe) 0.6
+        # angular.z: center (not actual max, but to be safe) 0.15
+
+
+
+        # max left and max right for SERVO
+        # left: 180, right: 90
+
+        # if no image (no mid), then stop, no throttle, no steering
+
+        # if center is within ~30px of actual middle, go straight
+
+
+
         self.steering_publisher.publish()
         self.throttle_publisher.publish(0.175)
         self.servo_publisher.publish()
--- testing/opencv_test.py
+++ testing/opencv_test.py
@@ -2,15 +2,16 @@ import cv2
 import numpy as np
 
 
-#start video capture
+# start video capture
 video = cv2.VideoCapture(0)
 
 while True:
+
     # current frame
     _, frame = video.read()
 
-    height, width = frame.shape[0:2]
-    print("height:", height, "width:", width)
+    # height, width = frame.shape[0:2]
+    # print("height:", height, "width:", width)
     # Image height: 480, Image width: 640
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -30,14 +31,14 @@ while True:
         c = max(contours, key=cv2.contourArea)
 
         x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
         # get midpoint of rectangle and print coordinates
         # (x,y) top left, (x+w, y+w) top right
-        mid_x = (x+w)/2
-        mid_y = y/2
+        mid_x = x + w/2
+        mid_y = y + h/2
         text = f"Center of rectangle: ({mid_x}, {mid_y})"
-        cv2.putText(frame, text, (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0))
+        cv2.putText(frame, text, (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255))
 
     # Display the frame image with bounded rectangle and masked image
     cv2.imshow('Video', frame)
